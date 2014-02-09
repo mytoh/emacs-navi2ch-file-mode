68ff35dbb8c702b2eb9d98dd81a068c4be1db034
mytoh <mytoh@gmail.com>
2014-02-10 07:12:25 +0900

    * navi2ch-file-mode.el: add top level file

---
+++ navi2ch-file-mode.el
@@ -0,0 +1,16 @@
+;;; navi2ch-file-mode.el -*- lexical-binding: t -*-
+
+(eval-when-compile
+  (require 'cl-lib))
+
+(req 'navi2ch-file-etc-mode)
+(req 'navi2ch-file-board-mode)
+(req 'navi2ch-file-dat-mode)
+(req 'navi2ch-file-subject-mode)
+
+(provide 'navi2ch-file-mode)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
